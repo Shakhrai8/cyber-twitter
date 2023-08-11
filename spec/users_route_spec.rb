48c314fab899a044dd847af1210d1355c7fecdf8
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-08-11 11:17:53 +0100

    updated test units for user route file, pass tests, 98% coverage

--- spec/users_route_spec.rb
+++ spec/users_route_spec.rb
@@ -37,6 +37,33 @@ RSpec.describe 'Users' do
       expect(last_response).to be_redirect
       expect(last_response.location).to include('/login')
     end
+
+    context 'with invalid inputs' do
+      it 'rejects empty name' do
+        post '/signup', user_params.merge(name: '')
+        expect(last_response).to be_redirect
+        expect(last_response.location).to include('/signup')
+        # You can further check for the error message in the session, if necessary
+      end
+  
+      it 'rejects empty username' do
+        post '/signup', user_params.merge(username: '')
+        expect(last_response).to be_redirect
+        expect(last_response.location).to include('/signup')
+      end
+  
+      it 'rejects invalid email format' do
+        post '/signup', user_params.merge(email: 'invalid_email')
+        expect(last_response).to be_redirect
+        expect(last_response.location).to include('/signup')
+      end
+  
+      it 'rejects password less than 8 characters' do
+        post '/signup', user_params.merge(password: 'short')
+        expect(last_response).to be_redirect
+        expect(last_response.location).to include('/signup')
+      end
+    end
   end
 
   describe 'POST /login' do
@@ -89,6 +116,17 @@ RSpec.describe 'Users' do
         expect(last_response).to be_ok
         expect(last_response.body).to include('Profile')
       end
+
+      it 'loads replies and notifications' do
+        # Mocking the methods
+        allow(ReplyRepository).to receive(:find_by_user).and_return([])
+        allow(NotificationRepository).to receive(:find_by_user).and_return([])
+  
+        get '/profile', {}, { 'rack.session' => { user_id: 'user_id' } }
+  
+        expect(ReplyRepository).to have_received(:find_by_user)
+        expect(NotificationRepository).to have_received(:find_by_user)
+      end
     end
     
 
@@ -98,6 +136,36 @@ RSpec.describe 'Users' do
         expect(last_response).to be_redirect
         expect(last_response.location).to include('/login')
       end
+    end
+  end
+
+  describe 'POST /update_photo' do
+    let(:photo_url) { 'http://example.com/photo.jpg' }
+  
+    it 'updates the user photo URL' do
+      expect(UserRepository).to receive(:update_photo).with('user_id', photo_url)
+      post '/update_photo', { photo_url: photo_url }.to_json, { 'rack.session' => { user_id: 'user_id' } }
+      expect(last_response.status).to eq(200)
+    end
+  end
+  
+  describe 'POST /update_bio' do
+    let(:bio) { 'This is a bio.' }
+  
+    it 'updates the user bio' do
+      expect(UserRepository).to receive(:update_bio).with('user_id', bio)
+      post '/update_bio', { bio: bio }.to_json, { 'rack.session' => { user_id: 'user_id' } }
+      expect(last_response.status).to eq(200)
+    end
+  end
+  
+  describe 'POST /profile/dismiss' do
+    it 'dismisses the notification' do
+      notification_id = 42
+      expect(NotificationRepository).to receive(:delete).with(notification_id)
+      post '/profile/dismiss', { notification_id: notification_id }, { 'rack.session' => { user_id: 'user_id' } }
+      expect(last_response).to be_redirect
+      expect(last_response.location).to include('/profile#notification-popup')
     end
   end
 
